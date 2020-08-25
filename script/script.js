b347f5690c6750c6b02c6d1c910e4d0e789f0dc3
FedericaC03 <fdrk03@hotmail.it>
2020-08-25 19:33:17 +0200

    Esercizio completato

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
 // CHIEDO ALL'UTENTE IL SUO COGNOME
-var cognome = prompt("Qual è il tuo cognome?");
+var cognome = prompt("Inserisci il tuo cognome");
 
-//LISTA COGNOMI REGISTRATI
-var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+//LISTA COGNOMI GIA' REGISTRATI
+var listaCognomi = ['Bianchi' , 'Rossi' , 'Duzioni' , 'Balsano' , 'Verdi' , 'Cortese' ];
+
+
+//METTO I COGNOMI IN ORDINE ALFABETICO
+listaCognomi.push(cognome);
+listaCognomi.sort();
+
+
+while(cognome.length <= 2){
+  cognome = prompt('Inserisci il tuo cognome');
+}
+
+for (var i = 0; i < listaCognomi.length; i++) {
+  document.getElementById("lista").innerHTML += '<li>' + listaCognomi[i] + '</li>';
+}
+
+document.getElementById("posizione").innerHTML = listaCognomi.indexOf(cognome) + 1;
