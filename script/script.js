c2080d8ecc7418a2c4a9f4f85806f50da77b4e56
FedericaC03 <fdrk03@hotmail.it>
2020-08-25 15:30:59 +0200

    inserito prompt e array

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// CHIEDO ALL'UTENTE IL SUO COGNOME
+var cognome = prompt("Qual è il tuo cognome?");
+
+//LISTA COGNOMI REGISTRATI
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
